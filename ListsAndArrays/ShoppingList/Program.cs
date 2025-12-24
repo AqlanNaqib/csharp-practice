61002c5be2a5b00679c7dfb8161f5e56f9e5b1e3
AqlanNaqib <aqlannaqib2@gmail.com>
2025-12-24 21:32:50 +0000

    Added the Shopping List practice by using list/ index removal and insert index

---
+++ ListsAndArrays/ShoppingList/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<string> ShoppingList = new List<string>();
+
+        ShoppingList.Add("Apple");
+        ShoppingList.Add("Banana");
+        ShoppingList.Add("Banana"); //mistake duplicate
+        ShoppingList.Add("Dumpling");
+
+       ShoppingList.RemoveAt(2); // remove the duplicate Banana and im using index removal
+       ShoppingList.Insert(2, "Carrot"); // insert Carrot at index 2
+
+       Console.WriteLine("Shopping List:");
+
+       foreach (string item in ShoppingList)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
