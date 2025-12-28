78de4e2a5122afee9707200143d393556d1546ba
AqlanNaqib <aqlannaqib2@gmail.com>
2025-12-28 23:10:08 +0000

    added simple nested loops practice

--- loops/starPyramid/Program.cs
+++ loops/starPyramid/Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 
 class Program
 {
     static void Main()
     {
-        
+        Console.WriteLine("Welcome to Star Pyramid");
+
+        for(int i=1; i<=5; i++)
+        {
+            for(int j=1; j<=i; j++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+        }
+
+
     }
 
 }
